c93a4754bf0453700132024894ca0b0490cf7aab
Miguel Benitez <benitezhm@gmail.com>
2018-08-04 09:26:55 -0400

    Writing public api in ExBanking module and write a few tests

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,18 +1,57 @@
 defmodule ExBanking do
   @moduledoc """
-  Documentation for ExBanking.
+  ExBanking simple banking OTP application.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
-
+  @typdoc """
+  Defining custom type for functions with error result
   """
-  def hello do
-    :world
+  @type banking_error ::
+          {:error,
+           :wrong_arguments
+           | :user_already_exists
+           | :user_does_not_exist
+           | :not_enough_money
+           | :sender_does_not_exist
+           | :receiver_does_not_exist
+           | :too_many_requests_to_user
+           | :too_many_requests_to_sender
+           | :too_many_requests_to_receiver}
+
+  @spec create_user(user :: String.t()) :: :ok | banking_error
+  def create_user(user) do
+    # TODO implement it
+    :ok
+  end
+
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number} | banking_error
+  def deposit(user, amount, currency) do
+    # TODO implement it
+    {:ok, 0.0}
+  end
+
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number} | banking_error
+  def withdraw(user, amount, currency) do
+    # TODO implement it, becarefull when subtract amount, should not allow negatives
+    {:ok, 0.0}
+  end
+
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number} | banking_error
+  def get_balance(user, currency) do
+    # TODO implement it
+    {:ok, 0.0}
+  end
+
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) :: {:ok, from_user_balance :: number, to_user_balance :: number} | banking_error
+  def send(from_user, to_user, amount, currency) do
+    # TODO implement it, becarefulll when subtract the amount, should not allow nevatives
+    {:ok, 0.0, 0.0}
   end
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -2,7 +2,24 @@ defmodule ExBankingTest do
   use ExUnit.Case
   doctest ExBanking
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  test "create user" do
+    assert ExBanking.create_user("miguel") == :ok
+  end
+
+  test "deposit" do
+    assert ExBanking.deposit("miguel", 0.0, "$") == {:ok, 0.0}
+  end
+
+  test "withdraw" do
+    assert ExBanking.withdraw("miguel", 0.0, "$") == {:ok, 0.0}
+  end
+
+  test "get balance" do
+    assert ExBanking.get_balance("miguel", "$") == {:ok, 0.0}
+  end
+
+  test "send" do
+    ExBanking.create_user("other")
+    assert ExBanking.send("miguel", "other", 0.0, "$") == {:ok, 0.0, 0.0}
   end
 end
