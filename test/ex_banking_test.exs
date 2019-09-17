2499c3725cf422d4f7338f16c258be0170f505ab
Miguel Benitez <benitezhm@gmail.com>
2019-09-16 21:20:18 -0400

    Improve Unit Tests

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -1,97 +1,117 @@
 defmodule ExBankingTest do
   use ExUnit.Case, async: true
-  require IEx
+  alias Decimal
   doctest ExBanking
 
+  @first_user "first user"
+  @second_user "second user"
+
+  setup_all do
+    ExBanking.create_user(@first_user)
+    ExBanking.create_user(@second_user)
+
+    :ok
+  end
+
   test "create user" do
-    assert ExBanking.create_user("user1") == :ok
-    assert ExBanking.create_user("user1") == {:error, :user_already_exists}
+    assert :ok == ExBanking.create_user("user")
+  end
+
+  test "user already exists" do
+    assert {:error, :user_already_exists} == 
+            ExBanking.create_user(@first_user)
   end
 
   test "deposit" do
-    assert ExBanking.create_user("user2") == :ok
-    balance = 0.0
-    assert ExBanking.get_balance("user2", "eur") == {:ok, balance}
-    balance = balance + 2
-    assert ExBanking.deposit("user2", 2, "eur") == {:ok, balance}
-    balance = balance + 3.0
-    assert ExBanking.deposit("user2", 3.0, "eur") == {:ok, balance}
-    balance = Float.round(balance + 3.789, 2)
-    assert ExBanking.deposit("user2", 3.789, "eur") == {:ok, balance}
+    assert {:ok, balance} = ExBanking.get_balance(@first_user, "eur")
+    assert {:ok, balance} = ExBanking.deposit(@first_user, 2.0, "eur")
   end
 
   test "deposit for a user that does not exist" do
-    assert ExBanking.deposit("user3", 10, "usd") == {:error, :user_does_not_exist}
+    assert {:error, :user_does_not_exist} ==
+            ExBanking.deposit("Not existing user", 10, "usd")
   end
 
   test "get balance" do
-    assert ExBanking.create_user("user4") == :ok
-    balance = Float.round(3.789, 2)
-    assert ExBanking.deposit("user4", 3.789, "usd") == {:ok, balance}
-    assert ExBanking.get_balance("user4", "usd") == {:ok, balance}
+    assert {:ok, balance} = ExBanking.get_balance(@first_user, "usd")
   end
 
   test "get balance for a user that does not exists" do
-    assert ExBanking.get_balance("user5", "usd") == {:error, :user_does_not_exist}
+    assert {:error, :user_does_not_exist} == 
+            ExBanking.get_balance("Not existing user", "usd")
+  end
+
+  test "withdrawal of the user bank account not enough money" do
+    {:ok, balance} = ExBanking.get_balance(@first_user, "usd")
+    withdraw_amt = balance + 1.0
+    assert {:error, :not_enough_money} == 
+            ExBanking.withdraw(@first_user, withdraw_amt, "usd") 
   end
 
   test "withdrawal of the user bank account" do
-    assert ExBanking.create_user("user6") == :ok
-    deposit_amount = 10
-    withdraw_amount = 5
-    assert ExBanking.withdraw("user6", withdraw_amount, "usd") == {:error, :not_enough_money}
-    assert ExBanking.deposit("user6", deposit_amount, "usd") == {:ok, deposit_amount}
-    balance = Float.round(deposit_amount / 1 - withdraw_amount, 2)
-    assert ExBanking.withdraw("user6", withdraw_amount, "usd") == {:ok, balance}
-    assert ExBanking.get_balance("user6", "usd") == {:ok, balance}
-    assert ExBanking.get_balance("user6", "eur") == {:ok, 0.0}
-    assert ExBanking.withdraw("user7", withdraw_amount, "usd") == {:error, :user_does_not_exist}
+    assert {:ok, _deposit_amt} = ExBanking.deposit(@first_user, 10.0, "usd") 
+    assert {:ok, balance} = ExBanking.withdraw(@first_user, 5.0, "usd")
   end
 
-  test "send" do
-    assert ExBanking.send("user_from", "user_to", 10.0, "usd") == {:error, :sender_does_not_exist}
-    ExBanking.create_user("user_from")
-    assert ExBanking.send("user_from", "user_to", 10.0, "usd") == {:error, :not_enough_money}
-    assert ExBanking.deposit("user_from", 20.0, "usd") == {:ok, 20.0}
-
-    assert ExBanking.send("user_from", "user_to", 10.0, "usd") ==
-             {:error, :receiver_does_not_exist}
-
-    ExBanking.create_user("user_to")
-    assert ExBanking.send("user_from", "user_to", 5.0, "usd") == {:ok, 15.0, 5.0}
-    assert ExBanking.get_balance("user_from", "usd") == {:ok, 15.0}
-    assert ExBanking.get_balance("user_to", "usd") == {:ok, 5.0}
+  test "withdrawal for a user that does not exists" do
+    assert {:error, :user_does_not_exist} ==
+            ExBanking.withdraw("Not existing user", 1.0, "usd")
   end
 
-  test "limit requests" do
-    ExBanking.create_user("user8")
-    1..800_000 |> Enum.each(fn _ -> spawn(fn -> ExBanking.deposit("user8", 1, "usd") end) end)
-    assert ExBanking.get_balance("user8", "usd") == {:error, :too_many_requests_to_user}
-    ExBanking.create_user("user9")
-    assert ExBanking.get_balance("user9", "usd") == {:ok, 0.0}
+  test "send money from a user that does not exists" do
+    assert {:error, :sender_does_not_exist} ==
+            ExBanking.send("Not existing user", @second_user, 10.0, "usd")
+  end
+
+  test "send money from user with not enough money" do
+    assert {:error, :not_enough_money} == 
+            ExBanking.send(@first_user, @second_user, 100.0, "usd")
+  end
+
+  test "send money to user that does not exists" do
+    ExBanking.deposit(@first_user, 1.0, "usd")
+    assert {:error, :receiver_does_not_exist} == 
+            ExBanking.send(@first_user, "Not existing user", 1.0, "usd")
+    
+  end
+
+  test "send money" do
+    ExBanking.deposit(@first_user, 10.0, "usd")
+    {_, balance} = ExBanking.get_balance(@first_user, "usd")
+    withdraw_amt = 5.0
+    new_balance = balance - withdraw_amt
+    assert {:ok, new_balance, withdraw_amt} == 
+            ExBanking.send(@first_user, @second_user, withdraw_amt, "usd")
   end
 
   test "limit transfer request" do
-    ExBanking.create_user("user10")
-    ExBanking.create_user("user11")
+    ExBanking.create_user("first_user_limit")
+    ExBanking.create_user("second_user_limit")
     # Test limit request fof sender
-    1..800_000 |> Enum.each(fn _ -> spawn(fn -> ExBanking.deposit("user10", 1, "usd") end) end)
+    1..800_000 |> Enum.each(fn _ -> spawn(fn -> 
+              ExBanking.deposit("first_user_limit", 1.0, "usd") end) end)
 
-    assert ExBanking.send("user10", "user11", 100, "usd") ==
-             {:error, :too_many_requests_to_sender}
+    assert {:error, :too_many_requests_to_sender} == 
+            ExBanking.send("first_user_limit", "second_user_limit", 100.0, "usd")
 
     # Test lmit request for receiver
-    1..800_000 |> Enum.each(fn _ -> spawn(fn -> ExBanking.deposit("user11", 1, "usd") end) end)
+    1..800_000 |> Enum.each(fn _ -> spawn(fn -> 
+              ExBanking.deposit("second_user_limit", 1.0, "usd") end) end)
 
-    assert ExBanking.send("user10", "user11", 100, "usd") ==
-             {:error, :too_many_requests_to_receiver}
+    assert {:error, :too_many_requests_to_receiver} == 
+            ExBanking.send("first_user_limit", "second_user_limit", 100.0, "usd")
+             
+  end
+
+  test "limit requests" do
+    ExBanking.create_user("third_user_limit")
+    1..800_000 |> Enum.each(fn _ -> spawn(fn -> ExBanking.deposit("third_user_limit", 1, "usd") end) end)
+    assert ExBanking.get_balance("third_user_limit", "usd") == {:error, :too_many_requests_to_user}
   end
 
   test "negative amount not allowed" do
-    ExBanking.create_user("user13")
-    ExBanking.create_user("user14")
-    assert ExBanking.deposit("user13", -1, "usd") == {:error, :wrong_arguments}
-    assert ExBanking.send("user13", "user14", -10, "usd") == {:error, :wrong_arguments}
-    assert ExBanking.withdraw("user13", -10, "usd") == {:error, :wrong_arguments}
+    assert ExBanking.deposit(@first_user, -1, "usd") == {:error, :wrong_arguments}
+    assert ExBanking.send(@first_user, @second_user, -10, "usd") == {:error, :wrong_arguments}
+    assert ExBanking.withdraw(@second_user, -10, "usd") == {:error, :wrong_arguments}
   end
 end
